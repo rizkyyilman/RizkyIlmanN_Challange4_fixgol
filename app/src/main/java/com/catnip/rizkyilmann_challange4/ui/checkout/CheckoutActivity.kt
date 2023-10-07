864277be9013d770b553a08a2d055bc7cd429b14
rizkyyilman <nuugrahaaa@gmail.com>
2023-10-08 02:31:25 +0700

    adding checkout activity and layouting for cart list

---
+++ app/src/main/java/com/catnip/rizkyilmann_challange4/ui/checkout/CheckoutActivity.kt
@@ -0,0 +1,12 @@
+package com.catnip.rizkyilmann_challange4.ui.checkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.catnip.rizkyilmann_challange4.R
+
+class CheckoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkout)
+    }
+}
